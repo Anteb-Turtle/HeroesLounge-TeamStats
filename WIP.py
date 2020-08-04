8274ccc6bcc6e919ff6856aa9bcb802f67c8eaa7
Anteb-Turtle <53829920+Anteb-Turtle@users.noreply.github.com>
2020-08-04 15:02:34 +0200

    Create WIP.py

---
+++ WIP.py
@@ -0,0 +1,15 @@
+import plotly.express as px
+
+#Tidying dataframe
+disp1 = pd.melt(team_display.df_map_wr.reset_index(), id_vars=['index'], var_name='hero', value_name='WR')
+disp2 = pd.melt(team_display.df_map_played.reset_index(), id_vars=['index'], var_name='hero', value_name='played')
+disp3 = pd.melt(team_display.df_bans_map.reset_index(), id_vars=['index'], var_name='hero', value_name='banned')
+disp = pd.merge(pd.merge(disp1,disp2, how='outer'),disp3, how='outer')
+disp = disp.fillna(0)
+disp.groupby(by=['index','hero'], as_index=False).sum()
+disp['ocurence'] = disp['played'] + disp['banned']
+
+
+#plotting
+fig = px.scatter(disp, x='hero', y='index', size='WR', color='ocurence')
+fig.show()
