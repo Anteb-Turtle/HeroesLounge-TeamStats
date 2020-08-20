d87ff00c98621d101fbd2bd7706d982ac1c8a617
Anteb-Turtle <53829920+Anteb-Turtle@users.noreply.github.com>
2020-08-20 10:23:20 +0200

    WIP testing

--- sanstitre1.py
+++ sanstitre1.py
@@ -8,6 +8,7 @@ Created on Thu Aug 20 10:00:11 2020
 import teamclasses as tc
 import ipywidgets as widgets
 from ipywidgets import interact, interact_manual, interactive
+from IPython.display import display
 import plotly.express as px
 import pandas as pd
 import numpy as np
@@ -16,6 +17,7 @@ import matplotlib.pyplot as plt
 class TeamWidget():
     
     def __init__(self, tag = 'TT', name='Turtle Team', n = [0]):
+        print('init')
         self.team_tag = widgets.Text(tag)
         self.team_name = widgets.Text(name)
         self.seasons = widgets.SelectMultiple(options=[0,1,2,3,4,5,6,7,8,9],
@@ -24,9 +26,11 @@ class TeamWidget():
                                         disabled=False)
         button = widgets.Button(description="Submit and run")
         button.on_click(self.submit_input)
+        display(self.team_tag, self.team_name, self.seasons, button)
+        print('display')
         
     def submit_input(self):
-
+        print('click')
         team_data = tc.TeamRawData(self.team_tag, self.team_name)
         team_data.set_seasons(self.seasons)
         ## Import data
